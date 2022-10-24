fb79719755049d2adb046b8de1aef429c6360975
Clara Hobbs <clara@clarahobbs.com>
2022-10-24 16:35:16 -0400

    Allow comparison of MeetRow vs MeetAny constraints
    Following the work of Vreman, Pates, and Maggio, it is possible to
    compare MeetRow and MeetAny constraints.  This is now implemented as two
    further methods of the <= function.

--- src/weaklyhard.jl
+++ src/weaklyhard.jl
@@ -25,7 +25,7 @@ struct MeetAny{T} <: WeaklyHardConstraint{T}
 end
 
 """
-    MeetRow{T}(meet::T,window::T)
+    MeetRow{T}(meet::T, window::T)
 
 Weakly hard constraint specifying that a task meets `meet` deadlines in a row in any window
 of size `window`.
@@ -46,6 +46,10 @@ end
 
 Construct a [`MeetAny{T}`](@ref) constraint that allows at most `miss` deadlines to be
 missed in any window of size `window`.
+
+While constraints of this sort are often expressed as a different type in the literature,
+they are equivalent to [`MeetAny`](@ref), so using a single type simplifies any code using
+weakly hard constraints.
 """
 MissAny(T::DataType, miss::Integer, window::Integer) = MeetAny{T}(window - miss, window)
 MissAny(miss::Int, window::Int) = MeetAny{Int}(window - miss, window)
@@ -75,3 +79,13 @@ import Base.<=
 <=(c::MeetAny, d::MeetAny) = d.meet <= maximum([floor(d.window/c.window) * c.meet, d.window + ceil(d.window/c.window) * (c.meet - c.window)])
 <=(c::MeetRow, d::MeetRow) = (d.window < c.window && d.meet <= c.meet - ceil((c.window - d.window)/2)) || (d.window >= c.window && d.meet <= c.meet)
 <=(c::MissRow, d::MissRow) = c.miss <= d.miss
+
+# Due to Vreman, Pates, and Maggio
+function <=(c::MeetRow, d::MeetAny)
+    p = c.window - c.meet + 1
+    d.meet <= c.meet * floor(d.window / p) + maximum([0, c.meet - p + d.window % p])
+end
+function <=(c::MeetAny, d::MeetRow)
+    z = c.window - c.meet
+    d.meet <= minimum([floor(d.window / (z + 1)), ceil(c.meet/z)])
+end
