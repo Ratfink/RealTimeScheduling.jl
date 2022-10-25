511bd209b87dbc9c67343bdc6a132dab81b1dda8
Clara Hobbs <clara@clarahobbs.com>
2022-10-25 14:42:33 -0400

    Add tests for FP schedulability

--- src/schedulability.jl
+++ src/schedulability.jl
@@ -3,6 +3,26 @@
 
 Check if the [`AbstractRealTimeTaskSystem`](@ref) `T` is schedulable by a fixed priority
 scheduler, with tasks prioritized by index (low to high).
+
+# Examples
+
+Lehoczky's counterexample to use of traditional TDA when deadlines exceed periods:
+
+```jldoctest lehoczky
+julia> ts = TaskSystem([PeriodicTask(70, 70, 26), PeriodicTask(100, 118, 62)]);
+
+julia> schedulable_fixed_priority(ts)
+true
+```
+
+Shortening the deadline of task 2 makes the system unschedulable:
+
+```jldoctest lehoczky
+julia> ts[2] = PeriodicTask(100, 116, 62);
+
+julia> schedulable_fixed_priority(ts)
+false
+```
 """
 function schedulable_fixed_priority(T::AbstractRealTimeTaskSystem)
     # First run a utilization test, due to Liu and Layland
--- test/runtests.jl
+++ test/runtests.jl
@@ -74,4 +74,22 @@ using Test
         @test eltype([τ_1, τ_f4]) == PeriodicTask{Float64}
         @test eltype([τ_f1, τ_4]) == PeriodicTask{Float64}
     end
+
+    @testset "Schedulability tests" begin
+        # Test the utilization bound
+        T = TaskSystem([PeriodicImplicitTask(5, 1), PeriodicImplicitTask(6, 2), PeriodicImplicitTask(8, 1)])
+        @test schedulable_fixed_priority(T)
+        T[1] = PeriodicImplicitTask(5, 3)
+        @test !schedulable_fixed_priority(T)
+        # Test simple TDA
+        T[1] = PeriodicImplicitTask(4, 2)
+        @test !schedulable_fixed_priority(T)
+        T[1] = PeriodicImplicitTask(2, 1)
+        @test schedulable_fixed_priority(T)
+        # Test general TDA
+        lehoczky = TaskSystem([PeriodicTask(70, 70, 26), PeriodicTask(100, 118, 62)])
+        @test schedulable_fixed_priority(lehoczky)
+        lehoczky[2] = PeriodicTask(100, 116, 62)
+        @test !schedulable_fixed_priority(lehoczky)
+    end
 end
