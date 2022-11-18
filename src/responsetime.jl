a152dede7b88c7e9ac5faaeee8d1e66fbf13698a
Clara Hobbs <clara@clarahobbs.com>
2022-11-18 15:24:14 -0500

    WIP: add compliant vector analysis
    I thought I'd implemented the algorithm right, but the answers are all
    larger than GEDFDeviAnderson.  That can't be right, since Devi and
    Anderson's method essentially computes a non-minimum compliant vector
    with all entries equal.  I'll have to find the problem later.

--- src/responsetime.jl
+++ src/responsetime.jl
@@ -15,9 +15,9 @@ const GEDFDeviAnderson = GEDFDeviAndersonAlg()
 """
     GEDFCompliantVector
 
-Indicate that a response time bound should be computed according to Erickson, Devi, and
-Baruah, "Improved Tardiness Bounds for Global EDF."
-DOI: https://doi.org/10.1109/ECRTS.2010.25
+Indicate that a response time bound should be computed according to Erickson, Guan, and
+Baruah, "Tardiness Bounds for Global EDF with Deadlines Different from Periods."
+DOI: https://doi.org/10.1007/978-3-642-17653-1_22
 """
 const GEDFCompliantVector = GEDFCompliantVectorAlg()
 
@@ -27,7 +27,7 @@ const GEDFCompliantVector = GEDFCompliantVectorAlg()
 Compute response time bounds for each task in the [`TaskSystem`](@ref) `T` under GEDF
 scheduling on `m` processors, according to the specified algorithm.
 """
-function response_time_gedf(T::TaskSystem{<:PeriodicImplicitTask}, m::Integer, alg::GEDFAlgorithm)
+function response_time_gedf(T::TaskSystem{<:AbstractRealTimeTask}, m::Integer, alg::GEDFAlgorithm)
     return tardiness_gedf(T, m, alg) .+ deadline.(T)
 end
 
@@ -54,4 +54,63 @@ function tardiness_gedf(T::TaskSystem{<:PeriodicImplicitTask}, m::Integer, ::GED
         x = (sum(cost, ϵ[1:Λ]) - cost(ϵ[end])) / (m - sum(utilization, μ[1:Λ-1]))
     end
     return x .+ cost.(T)
+end
+
+_S(τ::AbstractRealTimeTask) = cost(τ) * max(0, 1 − deadline(τ) / period(τ))
+_S(T::AbstractRealTimeTaskSystem) = sum(_S, T)
+
+"""
+    tardiness_gedf(T, m, ::GEDFCompliantVectorAlg)
+
+Compute tardiness bounds for each task in the [`TaskSystem`](@ref) `T` under GEDF
+scheduling on `m` processors, according to the `GEDFCompliantVector` algorithm.
+"""
+function tardiness_gedf(T::TaskSystem{<:AbstractRealTimeTask}, m::Integer, ::GEDFCompliantVectorAlg)
+    m > 1 || throw(ArgumentError("m must be at least 2"))
+    utilization(T) <= m || throw(ArgumentError("T must be feasible on m processors"))
+    utilization(T) > 1 || return zeros(length(T))
+
+    # Algorithm 1 from Erickson, Guan, and Baruah
+    m0 = ceil(Int, utilization(T))
+    _z(i, j) = (cost(i) + (cost(j)*utilization(j)) / m + (-cost(i)*utilization(i)) / m - cost(j)) / (utilization(j) - utilization(i))
+    points = Tuple{Float64, AbstractRealTimeTask, AbstractRealTimeTask}[]
+    for i in eachindex(T)
+        for j in 1:i-1
+            if utilization(T[i]) != utilization(T[j])
+                push!(points, (_z(T[i], T[j]), T[i], T[j]))
+            end
+        end
+    end
+
+    z0 = 0
+    pind = 1
+    if pind <= length(points)
+        z1, i, j = points[pind]
+        pid += 1
+    else
+        z1 = Inf
+    end
+
+    _sumterm(τ, z=0) = utilization(τ) * (z - cost(τ) / m) + cost(τ)
+    Θ = sort(T, by=_sumterm, rev=true)[1:m0]
+    zstar = 0
+    while true
+        @info "status" zstar z0 z1
+        zstar = ((_S(T) - sum(τ -> cost(τ) * (1 - utilization(τ) / m), Θ))
+                 / (sum(utilization, Θ) - m))
+        if z0 <= zstar <= z1
+            break
+        elseif i ∈ Θ && j ∉ Θ && utilization(j) > utilization(i)
+            Θ[findfirst(task -> task == i, Θ)] = j
+        end
+        z0 = z1
+        if pind <= length(points)
+            z1, i, j = points[pind]
+            pind += 1
+        else
+            z1 = Inf
+        end
+    end
+
+    return zstar .- cost.(T) ./ m .+ cost.(T)
 end
