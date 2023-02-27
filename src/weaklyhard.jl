df8ce109c2c3dba47a6981d8b12717a0c0ee1c0e
Shengjie Xu <shengjiex98@gmail.com>
2023-02-27 18:13:58 -0500

    WIP: Sampler for MeetAny constraint

--- src/weaklyhard.jl
+++ src/weaklyhard.jl
@@ -161,8 +161,9 @@ function <=(c::MeetAny, d::MeetRow)
     d.meet <= minimum([floor(d.window / (z + 1)), ceil(c.meet/z)])
 end
 
+abstract type SamplerWeaklyHard <: Random.Sampler{BitVector} end
 
-struct SamplerUniformMissRow <: Random.Sampler{BitVector}
+struct SamplerUniformMissRow <: SamplerWeaklyHard
     constraint::MissRow
     H::Int64
     l::Matrix{BigInt}
@@ -229,6 +230,51 @@ function Random.rand!(rng::Random.AbstractRNG, a::BitVector, sp::SamplerUniformM
 end
 
 function Random.rand(rng::Random.AbstractRNG, sp::SamplerUniformMissRow)
+    a = falses(sp.H)
+    Random.rand!(rng, a, sp)
+end
+
+struct SamplerUniformMeetAny <: SamplerWeaklyHard
+    constraint::MeetAny
+    H::Int64
+    l::Matrix{BigInt}
+end
+
+"""
+    SamplerUniformMeetAny(constraint::MeetAny, H::Int64)
+
+Pre-compute data for uniformly sampling `BitVector` objects from a [`MeetAny`](@ref)
+constraint. The sampled vectors will have length `H`.
+
+The algorithm used is due to Bernardi, Olivier, and Omer Giménez, "A linear algorithm for
+the random sampling from regular languages." Algorithmica 62.1 (2012): 130-145.
+
+# Examples
+
+```julia-repl
+julia> sp = SamplerUniformMissRow(MeetAny(1, 3), 10);
+
+julia> rand(sp)
+10-element BitVector:
+1
+1
+0
+1
+1
+0
+1
+0
+0
+1
+```
+"""
+function SamplerUniformMeetAny(constraint::MeetAny, H::Int64)
+end
+
+function Random.rand!(rng::Random.AbstractRNG, a::BitVector, sp::SamplerUniformMeetAny)
+end
+
+function Random.rand(rng::Random.AbstractRNG, sp::SamplerUniformMeetAny)
     a = falses(sp.H)
     Random.rand!(rng, a, sp)
 end
